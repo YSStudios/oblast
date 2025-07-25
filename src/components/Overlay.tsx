2e4d3edec18458e8ef6948c8d81173c4fac5daaf
Kirill Ginko <53977030+kirillginko@users.noreply.github.com>
2025-07-25 16:03:45 -0400

    updated overlay styling

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,65 +1,99 @@
-import React, { forwardRef, useCallback } from "react"
+import React, { forwardRef, useCallback } from "react";
 
 interface OverlayProps {
-  caption: React.MutableRefObject<HTMLSpanElement | null>
-  scroll: React.MutableRefObject<number>
+  caption: React.MutableRefObject<HTMLSpanElement | null>;
+  scroll: React.MutableRefObject<number>;
 }
 
-const Overlay = forwardRef<HTMLDivElement, OverlayProps>(({ caption, scroll }, ref) => {
-  const handleScroll = useCallback((e) => {
-    const scrollRatio = e.target.scrollTop / (e.target.scrollHeight - window.innerHeight)
-    scroll.current = Math.max(0, Math.min(1, scrollRatio))
-    if (caption.current) {
-      caption.current.innerText = scroll.current.toFixed(2)
-    }
-  }, [scroll, caption])
+const Overlay = forwardRef<HTMLDivElement, OverlayProps>(
+  ({ caption, scroll }, ref) => {
+    const handleScroll = useCallback(
+      (e) => {
+        const scrollRatio =
+          e.target.scrollTop / (e.target.scrollHeight - window.innerHeight);
+        scroll.current = Math.max(0, Math.min(1, scrollRatio));
+        if (caption.current) {
+          caption.current.innerText = scroll.current.toFixed(2);
+        }
+      },
+      [scroll, caption]
+    );
 
-  return (
-    <div
-      ref={ref}
-      onScroll={handleScroll}
-    className="scroll">
-    <div id="home" style={{ height: "200vh" }}>
-      <div className="dot">
-        {/* <h1>home</h1> */}
-        {/* Virtual reality (VR) is a simulated experience that can be similar to or completely different from the real world. */}
+    return (
+      <div ref={ref} onScroll={handleScroll} className="scroll">
+        <div id="home" style={{ height: "200vh" }}>
+          <div className="dot">
+            {/* <h1>home</h1> */}
+            {/* Virtual reality (VR) is a simulated experience that can be similar to or completely different from the real world. */}
+          </div>
+        </div>
+        <div id="what-we-do" style={{ height: "200vh" }}>
+          <div className="dot">
+            <h1>what we do</h1>
+            <p>
+              Headphones are a pair of small loudspeaker drivers worn on or
+              around the head over a user&apos;s ears.
+            </p>
+          </div>
+        </div>
+        <div id="our-process" style={{ height: "200vh" }}>
+          <div className="dot">
+            <h1>our process</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam
+              esse optio dolorum maiores eos quod, rem voluptas. Praesentium
+              tempora quod laudantium! Excepturi cumque dolore sapiente
+              consequuntur nostrum aliquam voluptatibus qui! Quae minus nostrum
+              nam cumque quam aut deleniti debitis ipsam dolor fugiat. Iure
+              assumenda, dolore minus praesentium recusandae architecto esse
+              laudantium nemo magni sed, rerum nam ut tenetur placeat cum.
+            </p>
+          </div>
+        </div>
+        <div id="team" style={{ height: "200vh" }}>
+          <div className="dot">
+            <h1>team</h1>
+            <p>Kirill Ginko & Sina Hassan</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
+              esse sequi iste cum dignissimos porro. Nisi veniam necessitatibus
+              impedit minima?
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+              sit magnam nisi temporibus laboriosam, libero tenetur. Voluptatem
+              perspiciatis porro sequi!
+            </p>
+          </div>
+        </div>
+        <div id="our-work" style={{ height: "200vh" }}>
+          <div className="dot">
+            <h1>our work</h1>
+            <p>Website 1</p>
+            <p>Website 2</p>
+            <p>Website 3</p>
+            <p>Website 4</p>
+            <p>Website 5</p>
+          </div>
+        </div>
+        <div id="contact" style={{ height: "200vh" }}>
+          <div className="dot">
+            <h1>contact</h1>
+            <p>NYC/BMORE</p>
+            <span>For Work Inquiries</span>
+            <p>EMAIL: info@oblast.studio</p>
+            <p>SOCIAL: @oblast.studio</p>
+            <p>TEL: +3015154239</p>
+          </div>
+        </div>
+        <span className="caption" ref={caption}>
+          0.00
+        </span>
       </div>
-    </div>
-    <div id="what-we-do" style={{ height: "200vh" }}>
-      <div className="dot">
-        <h1>what we do</h1>
-        Headphones are a pair of small loudspeaker drivers worn on or around the head over a user&apos;s ears.
-      </div>
-    </div>
-    <div id="our-process" style={{ height: "200vh" }}>
-      <div className="dot">
-        <h1>our process</h1>A rocket (from Italian: rocchetto, lit. &apos;bobbin/spool&apos;)[nb 1][1] is a projectile that spacecraft, aircraft or other vehicle use to obtain thrust from a
-        rocket engine.
-      </div>
-    </div>
-    <div id="team" style={{ height: "200vh" }}>
-      <div className="dot">
-        <h1>team</h1>The minds behind the madness.
-      </div>
-    </div>
-    <div id="our-work" style={{ height: "200vh" }}>
-      <div className="dot">
-        <h1>our work</h1>A turbine (/ˈtɜːrbaɪn/ or /ˈtɜːrbɪn/) (from the Greek τύρβη, tyrbē, or Latin turbo, meaning vortex)[1][2] is a rotary mechanical device that extracts energy
-        from a fluid flow and converts it into useful work.
-      </div>
-    </div>
-    <div id="contact" style={{ height: "200vh" }}>
-      <div className="dot">
-        <h1>contact</h1>A table is an item of furniture with a flat top and one or more legs, used as a surface for working at, eating from or on which to place things.[1][2]
-      </div>
-    </div>
-    <span className="caption" ref={caption}>
-      0.00
-    </span>
-  </div>
-  )
-})
+    );
+  }
+);
 
-Overlay.displayName = 'Overlay'
+Overlay.displayName = "Overlay";
 
-export default Overlay
+export default Overlay;
